73587447584d2578bbfb1462008766ebfe576428
Anton Bronnikov <anton@northernforest.nl>
2024-03-01 09:39:11 +0200

    wip

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,13 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import TwitterProvider from '@auth/core/providers/twitter';
 
+import {AUTH_SECRET, AUTH_TWITTER_ID, AUTH_TWITTER_SECRET} from "$env/static/private";
+
 export const { handle, signIn, signOut } = SvelteKitAuth({
     basePath: "/auth",
-    secret: process.env.AUTH_SECRET,
+    secret: AUTH_SECRET,
     providers: [TwitterProvider({
-        clientId: process.env.AUTH_TWITTER_ID,
-        clientSecret: process.env.AUTH_TWITTER_SECRET,
+        clientId: AUTH_TWITTER_ID,         // oauth 2.0 client id
+        clientSecret: AUTH_TWITTER_SECRET, // oauth 2.0 client secret
     })]
 });
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 
-export const prerender = true;
-
 export const load: LayoutServerLoad = async (event) => {
   return {
     session: await event.locals.getSession()
